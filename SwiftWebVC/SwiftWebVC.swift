a775e7f37db02bc05b01df513e0d0b52f03c8828
Charlie Fish <fishcharlie@users.noreply.github.com>
2017-04-17 16:33:57 -0600

    Update SwiftWebVC.swift

--- SwiftWebVC/SwiftWebVC.swift
+++ SwiftWebVC/SwiftWebVC.swift
@@ -134,7 +134,7 @@ public class SwiftWebVC: UIViewController {
         navBarTitle.shadowOffset = CGSize(width: 0, height: 1);
         navBarTitle.font = UIFont(name: "HelveticaNeue-Medium", size: 17.0)
         
-        if webView.loading == false {
+        if webView.isLoading == false {
             webView.evaluateJavaScript("document.title", completionHandler: {(response, error) in
                 self.navBarTitle.text = response as! String?
                 self.navBarTitle.sizeToFit()
